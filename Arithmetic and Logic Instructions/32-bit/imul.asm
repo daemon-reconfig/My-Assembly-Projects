15eb1e636856a33cb52bfbc83b479fe8421a00d2
Daemon <69844188+daemon-reconfig@users.noreply.github.com>
2023-09-01 18:17:20 +0530

    Update imul.asm
    improving the code

--- Arithmetic and Logic Instructions/32-bit/imul.asm
+++ Arithmetic and Logic Instructions/32-bit/imul.asm
@@ -2,18 +2,25 @@ section .data
     num1 dd 40
     num2 dd 20
     result dd ?
+    out_fmt db "Result: %d", 10, 0  ; Format string for printing the result
+
 section .text
     global _start
+
 _start:
     mov eax, [num1]
     mov ebx, [num2]
-    imul ebx, EAX
+    imul ebx, eax           ; Multiply num1 and num2, result in EBX
     mov [result], ebx
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, result
-    mov edx, 4
+
+    ; Prepare for printing
+    mov eax, 4               ; 4 is the sys_write syscall number
+    mov ebx, 1               ; 1 is the file descriptor for stdout
+    mov ecx, result          ; Load the address of result
+    mov edx, 4               ; Number of bytes to print (assuming a 32-bit result)
     int 80h
-    mov eax, 1
-    mov ebx, 0
+
+    ; Exit the program
+    mov eax, 1               ; 1 is the sys_exit syscall number
+    mov ebx, 0               ; Exit status (0 for success)
     int 80h

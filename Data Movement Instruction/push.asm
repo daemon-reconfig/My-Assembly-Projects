3e42af44dd785782b1b9f2c3ec571aae7c2fa3c5
Daemon <69844188+daemon-reconfig@users.noreply.github.com>
2023-09-01 16:31:15 +0530

    Update push.asm

--- Data Movement Instruction/push.asm
+++ Data Movement Instruction/push.asm
@@ -1,1 +1,23 @@
+section .data
+    arr dd 100 dup(0)
+    top dd 0
 
+section .text
+    global _start
+    _start: 
+        ;push values to stack
+        push_i 1
+        push_i 2
+        push_i 3
+        push_i 4
+        mov eax, 1
+        int 0x80
+    push_i:
+        mov eax, 0
+        mov ebx, 0
+        mov ecx, 0
+
+        mov al, [top]
+        mov [arr + ebx], al
+        inc [top]
+        ret

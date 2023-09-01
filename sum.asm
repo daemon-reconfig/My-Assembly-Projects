d981b5201bc04b82ba56a3ce5b454149ca13978a
Daemon <69844188+daemon-reconfig@users.noreply.github.com>
2023-09-01 14:14:28 +0530

    Add files via upload

---
+++ sum.asm
@@ -0,0 +1,12 @@
+section .data
+    num1 dd 20
+    num2 dd 10
+    result dd 0
+section .text
+global _start
+_start: 
+    mov eax, [num1]
+    add eax, [num2]
+    mov [result], eax
+    mov eax, 1
+    int 0x80
